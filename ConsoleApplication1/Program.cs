d02cc1ba23541eed771069ebe7d48294a6c0307d
Neil Jeric Dacayan <n.dacayan@gmail.com>
2016-10-17 07:41:08 +0800

    first push

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityModelLibrary;
+using DataAccess;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserManager manager = new UserManager();
+            manager.RetrieveUser();
+        }
+    }
+}
---
+++ DataAccess/Manager/Utility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Manager
+{
+    class Utility
+    {
+        public string GetString(byte[] input)
+        {
+            string output = "";
+            output = ASCIIEncoding.ASCII.GetString(input);
+            return output;
+        }
+
+        public byte[] GetBytes(string input)
+        {
+            byte[] output;
+            output = ASCIIEncoding.ASCII.GetBytes(input);
+            return output;
+        }
+    }
+}
